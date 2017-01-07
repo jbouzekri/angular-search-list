bc28a7e374b127a8bbb38d586fd559df2cbaa226
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2017-01-07 22:29:46 +0100

    clean gh pages

--- src/app/pagination/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Created by jobou on 07/01/17.
- */
-
-export { PaginationComponent } from './pagination.component';
--- src/app/search-list/api/list-result.interface.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by jobou on 07/01/17.
- */
-
-export interface ListResult<T> {
-  items: T[]
-
-  total: number
-}
