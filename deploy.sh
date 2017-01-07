ddee07c4cb5e15247fe0438377ad6587d637960d
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2017-01-07 22:36:53 +0100

    simple deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git clone git@github.com:jbouzekri/angular-search-list.git /tmp/angular-search-list
+COMMIT_HASH=`git rev-parse HEAD`
+echo $COMMIT_HASH
+cd /tmp/angular-search-list
+git checkout gh-pages
+cd -
+ng build
+cp -rf dist/* /tmp/angular-search-list/
+cd /tmp/angular-search-list
+git add .
+git commit -m "release : $COMMIT_HASH"
+git push
+cd -
